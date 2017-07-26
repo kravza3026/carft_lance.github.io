aa33d8332d216ba2305333f67a8feef04d390b27
kravza <kravza3026@gmail.com>
2017-07-26 15:53:15 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.slaider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'});
